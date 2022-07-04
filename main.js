26701a89a0f6bf31367e9b6e1991ff99a5fc5f46
Abhi Wadekar <abhi1@berkeley.com>
2022-07-03 19:09:27 -0700

    access node from renderer

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600
-    })
+const path = require('path')
 
-    win.loadFile('index.html')
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
